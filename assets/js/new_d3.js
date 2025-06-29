c823bbb3821ebc8e9529efd7cb1528ca35aa198c
JacobHobson1 <c1032346@newcastle.ac.uk>
2025-06-29 08:25:56 +0100

    bug fixes

--- assets/js/new_d3.js
+++ assets/js/new_d3.js
@@ -175,7 +175,7 @@ window.onload = async function () {
     .attr("d", line);
 
   // Add event markers (function start/end)
-  chartArea.selectAll(".event-line")
+  d3.selectAll(".event-line")
     .data(events)
     .enter()
     .append("line")
@@ -192,7 +192,7 @@ window.onload = async function () {
   const chartHeight = height - margin.bottom - margin.top;
   
   // Add start markers for each layer
-  chartArea.selectAll(".profile-start-line")
+  d3.selectAll(".profile-start-line")
     .data(profileEvents.slice(0, 15)) // Show first 15 layers for clarity
     .enter()
     .append("line")
@@ -213,7 +213,7 @@ window.onload = async function () {
     .attr("opacity", 0.7);
 
   // Add end markers for longer operations (optional, for duration visualization)
-  chartArea.selectAll(".profile-end-line")
+  d3.selectAll(".profile-end-line")
     .data(profileEvents.filter(d => d.duration > 10).slice(0, 10)) // Only show end lines for longer operations
     .enter()
     .append("line")
@@ -246,7 +246,7 @@ window.onload = async function () {
     .style("opacity", 0);
 
   // Add invisible circles for hover detection on energy data
-  chartArea.selectAll(".hover-circle")
+  d3.selectAll(".hover-circle")
     .data(data)
     .enter()
     .append("circle")
@@ -268,7 +268,7 @@ window.onload = async function () {
     });
 
   // Add hover for profile start lines
-  chartArea.selectAll(".profile-start-line")
+  d3.selectAll(".profile-start-line")
     .on("mouseover", function(event, d) {
       d3.select(this).attr("stroke-width", 3).attr("opacity", 1);
       tooltip.transition().duration(200).style("opacity", 1);
@@ -287,7 +287,7 @@ window.onload = async function () {
     });
 
   // Add hover for profile end lines
-  chartArea.selectAll(".profile-end-line")
+  d3.selectAll(".profile-end-line")
     .on("mouseover", function(event, d) {
       d3.select(this).attr("stroke-width", 3).attr("opacity", 1);
       tooltip.transition().duration(200).style("opacity", 1);
@@ -356,29 +356,29 @@ window.onload = async function () {
       path.attr("d", line.x(d => newX(d.milliseconds)));
       
       // Update event lines on zoom
-      chartArea.selectAll(".event-line")
+      d3.selectAll(".event-line")
         .attr("x1", d => newX(d.milliseconds))
         .attr("x2", d => newX(d.milliseconds));
 
       // Update profile start lines on zoom
-      chartArea.selectAll(".profile-start-line")
+      d3.selectAll(".profile-start-line")
         .attr("x1", d => newX(d.relativeStart))
         .attr("x2", d => newX(d.relativeStart));
 
       // Update profile end lines on zoom
-      chartArea.selectAll(".profile-end-line")
+      d3.selectAll(".profile-end-line")
         .attr("x1", d => newX(d.relativeEnd))
         .attr("x2", d => newX(d.relativeEnd));
 
       // Update hover circles on zoom
-      chartArea.selectAll(".hover-circle")
+      d3.selectAll(".hover-circle")
         .attr("cx", d => newX(d.milliseconds));
     });
 
   svg.call(zoom);
 };
 
-  chartArea.selectAll(".event-line")
+  d3.selectAll(".event-line")
     .on("mouseover", function(event, d) {
       tooltip.transition().duration(200).style("opacity", 1);
       tooltip.html(`<strong>Event:</strong> ${d.event}<br/>Time: ${d.milliseconds} ms`)
